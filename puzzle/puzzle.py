526f4fb6125a209bf3c8c029a234c9bde68a9a6f
Kristian Rother <kristian@spiced-academy.com>
2021-03-26 23:27:43 +0100

    annotate wrong merge

--- puzzle/puzzle.py
+++ puzzle/puzzle.py
@@ -6,10 +6,10 @@ a = np.array(Image.open('a.png'))
 b = np.array(Image.open('b.png'))
 print(a.shape, b.shape)
 
-im = np.vstack([a,b])  # vertical merge
+im = np.vstack([a,b])  # vertical merge (correct)
 print(im.shape)
 Image.fromarray(im).save('vertical.png')
 
-im = np.hstack([a,b])  # horizontal merge
+im = np.hstack([a,b])  # horizontal merge (WRONG)
 print(im.shape)
 Image.fromarray(im).save('horizontal.png')
