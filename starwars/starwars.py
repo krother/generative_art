e5f4f6016e356d321dc6d11009cafc488a8764ae
Kristian Rother <krother@academis.eu>
2022-07-05 16:05:59 +0200

    speedup by caching indexes

--- starwars/starwars.py
+++ starwars/starwars.py
@@ -17,39 +17,45 @@ msg = np.zeros((3000, 1400, 3), np.uint8)
 for y, line in enumerate(text):
     cv2.putText(msg, line.strip(), (50, y * 50 + 50), cv2.FONT_HERSHEY_TRIPLEX, fontScale=1.5, color=YELLOW, thickness=3)
 
-ofs = 0
 
+indices = {}
+
+# pre-calculate index arrays for each row
+# to speed up display
+c = 400
+for yy in range(1, 800):
+    y = 1 - yy / 800
+    src = []
+    dest = []
+    for xx in range(1400):
+        x = xx - 700
+        x0 = int(x - (x * y) / (y - 1))
+        y0 = int(-y * c / (y - 1))
+
+        x0 += 600
+        if 0 <= x0 < 1400:
+            src.append(x0)
+            dest.append(xx)
+    
+    src = np.array(src).astype(int)
+    dest = np.array(dest).astype(int)
+    indices[yy] = (y0, src, dest)
+
+ofs = 0
 frames = []
+
 while True:
     # display frames
     background = np.zeros((800, 1400, 3), np.uint8)
-    c = 400
-    src_indices = []
-    dest_indices = []
-    for xx in range(1400):
-        x = xx - 700
-        for yy in range(1, 800):
-            y = 1 - yy / 800
-            x0 = int(x - (x * y) / (y - 1))
-            y0 = int(-y * c / (y - 1))
-
-            x0 += 600
-            if (
-                0 <= x0 < 1400 and
-                0 <= y0 < 800
-            ):
-                src_indices.append((-y0*1400+x0))
-                dest_indices.append(yy*1400 + xx)
-                background[yy, xx] = msg[-y0 + ofs, x0]
+    for yy in range(1, 800):
+        y0, src, dest = indices[yy]
+        if 0 <= y0 < 800:
+            background[yy][dest] = msg[-y0 + ofs][src]
     
-    #src_indices = np.array(src_indices)
-    #print(len(dest_indices))
-    #background[tuple(dest_indices)] = 255#msg[tuple(src_indices + ofs*1400)]
-
     frame = background.copy()
     cv2.imshow('frame', frame)
 
-    ofs += 10
+    ofs += 1
 
     # shrink frame for using it on the web
     #rgb = cv2.cvtColor(frame[100:450:2, 200:-200:2], cv2.COLOR_BGR2RGB)
